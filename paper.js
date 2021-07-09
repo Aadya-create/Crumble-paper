11ef5dc7bd142e47e103f719736fb55679ee4520
Aadya-create <79462489+Aadya-create@users.noreply.github.com>
2021-07-09 19:53:57 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			restitution : 0.3,
+            density : 0.5,
+            friction : 0			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r;
+        this.image = loadImage("paper.png")
+		this.body=Bodies.circle(x, y, r, options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperPos=this.body.position;		
+
+			push()
+			translate(paperPos.x, paperPos.y);
+			imageMode(CENTER)
+			//strokeWeight(4);
+		
+			image(this.image,0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
